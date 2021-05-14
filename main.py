8b3c59fd2b02b9a1719a0e67473dc02de9f921a5
kubaszpak <jakub.szpak.it@gmail.com>
2021-05-14 14:24:07 +0200

    Abandoning bchlib

--- main.py
+++ main.py
@@ -23,27 +23,27 @@ def error_factor(generated_array, decoded_array):
 
 
 def main():
-    # start = time.time()
+    start = time.time()
 
     # a = []
     # b = []
 
     # for i in range(10, 25):
 
-    # generator = Generator(2**20)
-    # # print(generator.bit_array)
+    generator = Generator(2**20)
+    # print(generator.bit_array)
 
-    # coder = Coder(generator.bit_array)
+    coder = Coder(generator.bit_array)
 
     # # a.append(i)
 
-    # channel = Channel(coder.triple_array, 0.15)
-    # # print(coder.triple_array)
+    channel = Channel(coder.triple_array, 0.15)
+    # print(coder.triple_array)
 
-    # decoder = Decoder(channel.distorted_array)
-    # # print(decoder.decoded_array)
+    decoder = Decoder(channel.distorted_array)
+    # print(decoder.decoded_array)
 
-    # print(error_factor(generator.bit_array, decoder.decoded_array))
+    print(error_factor(generator.bit_array, decoder.decoded_array))
 
     # print(a, b)
     # plt.plot(a, b)
@@ -51,40 +51,40 @@ def main():
     # plt.xlabel("Sample size m as Exponent of two")
     # plt.ylabel("Error factor in %")
     # plt.show()
-    # end = time.time()
-    # print(end-start)
+    end = time.time()
+    print("Time:", end-start)
     """
     ETAP II
     """
 
-    # random bitarray len = 127
-    data = bitarray([random.randint(0, 1) for i in range(127)])
-    # data = bytearray(os.urandom(255))
+#     # random bitarray len = 127
+#     data = bitarray([random.randint(0, 1) for i in range(127)])
+#     # data = bytearray(os.urandom(255))
 
-    # create a bch object
-    BCH_POLYNOMIAL = 8219
-    BCH_BITS = 19
-    bch = bchlib.BCH(BCH_POLYNOMIAL, BCH_BITS)
-    print(f"bch.m: {bch.m}, bch.n: {bch.n}, bch.t: {bch.t}")
+#     # create a bch object
+#     BCH_POLYNOMIAL = 8219
+#     BCH_BITS = 19
+#     bch = bchlib.BCH(BCH_POLYNOMIAL, BCH_BITS)
+#     print(f"bch.m: {bch.m}, bch.n: {bch.n}, bch.t: {bch.t}")
 
-    ecc = bch.encode(data)
+#     ecc = bch.encode(data)
 
-    print(data, len(data))
-    print(ecc, len(ecc))
+#     print(data, len(data))
+#     print(ecc, len(ecc))
 
-    result_bits = bitarray()
+#     result_bits = bitarray()
 
-    result_bits.frombytes(bytes(ecc))
+#     result_bits.frombytes(bytes(ecc))
 
-    # print(data)
-    print(result_bits, len(result_bits))
-    # print(error_factor(data, result_bits))
+#     # print(data)
+#     print(result_bits, len(result_bits))
+#     # print(error_factor(data, result_bits))
 
-    packet = ecc + data
+#     packet = ecc + data
 
-    print('data size: %d' % (len(data)))
-    print('ecc size: %d' % (len(ecc)))
-    print('packet size: %d' % (len(packet)))
+#     print('data size: %d' % (len(data)))
+#     print('ecc size: %d' % (len(ecc)))
+#     print('packet size: %d' % (len(packet)))
 
 
 if __name__ == '__main__':
